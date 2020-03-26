7272ecf86c86b42090054280c13138d184cf9a7c
Udara ranasinghe <indi79@hotmail.com>
2020-03-26 13:17:13 +1030

    Adding Local Storage Feature

--- js/script.js
+++ js/script.js
@@ -1,43 +1,160 @@
 const Weather = {
 
-    properties :{
+    properties : {
         api_key : "08d6ce794cdf279b6219aa4a39a325d7",
         api_url_current : "https://api.openweathermap.org/data/2.5/weather?",
-        api_url_forcast : "https://api.openweathermap.org/data/2.5/forecast?"
+        api_url_forcast : "https://api.openweathermap.org/data/2.5/forecast?",
+        api_url_uv_index : "https://api.openweathermap.org/data/2.5/uvi?",
+        api_url_icons : 'http://openweathermap.org/img/wn/',
+        display_units : 'metric',
+        lattitude : '',
+        longitude : ''
     },
 
-     callAPI: async function(url,city) {
+    history : [],
+
+     callAPI: async function(url,query_string) {
         return $.ajax({
-            url: url +  `q=${city}&appid=` + this.properties.api_key,
-            type: 'GET',
-            data: {
-              key: 'value',
-            }
+            url: url +  query_string,
+            type: 'GET'
           });
       },
 
-      renderTodaysWeather: async function(response) {
-        consle.log(response);
+      renderTodaysWeather: function(response) {
+        $('#main-city').text(response.name). append(' ('). 
+        append( this.convertUnixTimeStampToDate(response.dt) ). append(')'). 
+        append(this.renderWeatherIcon(response.weather[0].icon));
+        $('#main-temperature').text(response.main.temp) . append(' °C');
+        $('#main-humidity').text(response.main.humidity) . append('%');
+        $('#main-windspeed').text(this.convertMeterstoKms(response.wind.speed));
       },
+
+      getCurrentWeather: function(city){
+          var query_string_weather = `units=${Weather.properties.display_units}&q=${city}&appid=${Weather.properties.api_key}`;
+          Weather.callAPI( Weather.properties.api_url_current , query_string_weather).then(response => {
+          Weather.renderTodaysWeather(response);
+          Weather.properties.lattitude =  response.coord.lat;
+          Weather.properties.longitude =  response.coord.lon; 
+          Weather.getUVIndex();
+          Weather.saveHistory($('#city').val());
+          Weather.populateHistory();
+        }) 
+        .catch(error => {
+          Weather.handleError(error.responseJSON.message);
+        });
+      },
+
+      getForecast: function(city){
+          var query_string_weather = `units=${Weather.properties.display_units}&q=${city}&appid=${Weather.properties.api_key}`;
+          Weather.callAPI( Weather.properties.api_url_forcast , query_string_weather).then(response => {
+            $('#five-day-forecast').html('');
+            for (let i = 0; i < response.list.length; i) {
+            $('#five-day-forecast').append(Weather.renderForcastBox(response.list[i]));
+            i = i + 8;
+            }
+        }) 
+        .catch(error => {
+          Weather.handleError(error.responseJSON.message);
+        })
+      },
+
+      getUVIndex: function(){
+        var query_string_uv_index = `appid=${this.properties.api_key}&lat=${this.properties.lattitude}&lon=${this.properties.longitude}`;
+        Weather.callAPI( Weather.properties.api_url_uv_index , query_string_uv_index).then(response => {
+          $('#main-uvindex').text(response.value);
+          var uvclass = ""
+          var uvvale = response.value;
+          if(uvvale < 2.9) { uvclass = 'green'; }
+          else if(uvvale > 3 && uvvale < 4.9) { uvclass = 'yellow'; }
+          else if(uvvale > 5 && uvvale < 7.9) { uvclass = 'orange'; }
+          else if(uvvale > 8 && uvvale < 10.9) { uvclass = 'red'; }
+          else { uvclass = 'darkred'; }
+          $('#main-uvindex').css("background-color", uvclass);
+        }) 
+        .catch(error => {
+          Weather.handleError(error.message);
+        });
+      },
+
+      renderForcastBox: function(response) {
+        var card = "";
+        card = '<div class="card col-12 col-sm-12 col-md-2 text-center"> <div class="card-body">';
+        card += `<p>${this.convertUnixTimeStampToDate(response.dt)}</p>`;
+        card += this.renderWeatherIcon(response.weather[0].icon);
+        card += `<p> ${response.main.temp} °C</p>`;   
+        card += '</div></div>';
+        return card;
+      },
+
+      convertMeterstoKms : function(meters) {
+        return ( Math.floor (meters * 3600/1000) ).toString() + 'Kms';
+      },
+
+      convertUnixTimeStampToDate: function(unix_time_stamp){
+        return moment.unix(unix_time_stamp).format("DD/MM/YYYY");
+      },
+
+      renderWeatherIcon: function(icon_code){
+        var full_url = this.properties.api_url_icons + icon_code +'@2x.png';
+        return `<img src ="${full_url}" class="weather-icon">`;
+      },
+
+      saveHistory : function(city){
+        if(!this.history.includes(city) && city !="") {  
+          this.history.push(city);
+          localStorage.setItem('weather_dashboard', JSON.stringify(this.history));
+        }
+      },
+
+      retrieveHistory : function()
+      {
+        if (localStorage.getItem('weather_dashboard')){
+          Weather.history =  JSON.parse(localStorage.getItem('weather_dashboard'));
+          Weather.populateHistory();
+          Weather.getCurrentWeather(Weather.history[Weather.history.length-1]);
+          Weather.getForecast(Weather.history[Weather.history.length-1]);
+        }
+      },
+
+      populateHistory : function(){
+        $('#search-history').html('');
+        for (let i = 0; i < this.history.length; i++) {
+          $('#search-history').append(`<div data-city="${this.history[i]}" class="search-history col-12 p-2 border rounded primary">${this.history[i]}</div>`);
+        }
+      },
+
+      handleError : function(error){
+        $('.error-body').html(error);
+        $('#myModal').modal('show');
+      }
 };
 
 
-$("#search").on( "click", function() {
+$( document ).ready(function() {
 
-  Weather.callAPI( Weather.properties.api_url_current ,'Adelaide').then(response => {
-    console.log(response);
-  }) 
-  .catch(error => {
-    console.log(error.status);
-  })
+  Weather.retrieveHistory();
 
-  Weather.callAPI( Weather.properties.api_url_forcast ,'Adelaide').then(response => {
-    console.log(response);
-  }) 
-  .catch(error => {
-    console.log(error.status);
-  })
+  function triggerAPICalls(city)
+  {
+    Weather.getCurrentWeather(city);
+    Weather.getForecast(city);
+  }
+
+  $('body').on('click', '.search-history', function() {
+    triggerAPICalls($(this).attr("data-city"));
+  });
+
+  $("#search").on( "click", function() {
+    if($('#city').val().length >= 4)
+    {
+      triggerAPICalls($('#city').val());
+    }
+    else {
+      Weather.handleError('City must be more than 2 characters long');
+    }
+  });
 
 });
+
 
 
