d7115bbd049938e2680cfec5b689316deb881d5c
Udara ranasinghe <indi79@hotmail.com>
2020-03-30 12:21:46 +1030

    Addnig comments

--- js/script.js
+++ js/script.js
@@ -11,8 +11,10 @@ const Weather = {
         longitude : ''
     },
 
+    /* saveHistory function poulate history [] so retrieveHistory fucntion can use it to populate search history*/
     history : [],
 
+    /* take url of an API and query parameter string to return a response */
      callAPI: async function(url,query_string) {
         return $.ajax({
             url: url +  query_string,
@@ -20,6 +22,7 @@ const Weather = {
           });
       },
 
+      /*Display current weather of the City being searched*/
       renderTodaysWeather: function(response) {
         $('#main-city').text(response.name). append(' ('). 
         append( this.convertUnixTimeStampToDate(response.dt) ). append(')'). 
@@ -29,32 +32,33 @@ const Weather = {
         $('#main-windspeed').text(this.convertMeterstoKms(response.wind.speed));
       },
 
+      
       getCurrentWeather: function(city){
           var query_string_weather = `units=${Weather.properties.display_units}&q=${city}&appid=${Weather.properties.api_key}`;
           Weather.callAPI( Weather.properties.api_url_current , query_string_weather).then(response => {
           Weather.renderTodaysWeather(response);
-          Weather.properties.lattitude =  response.coord.lat;
-          Weather.properties.longitude =  response.coord.lon; 
+          Weather.properties.lattitude =  response.coord.lat; // assigning lattitude and longitude ptoperties 
+          Weather.properties.longitude =  response.coord.lon; // so it can be used by getUVIndex() below
           Weather.getUVIndex();
-          Weather.saveHistory($('#city').val());
-          Weather.populateHistory();
+          Weather.saveHistory($('#city').val()); // save current city to search history
+          Weather.populateHistory(); // populate latest search history
         }) 
         .catch(error => {
-          Weather.handleError(error.responseJSON.message);
+          Weather.handleError(error.responseJSON.message); // if an error occurs call handleError fucntion with the error message
         });
       },
 
       getForecast: function(city){
           var query_string_weather = `units=${Weather.properties.display_units}&q=${city}&appid=${Weather.properties.api_key}`;
           Weather.callAPI( Weather.properties.api_url_forcast , query_string_weather).then(response => {
-            $('#five-day-forecast').html('');
+            $('#five-day-forecast').html(''); // reset #five-day-forecast div to nothing before populating with latest forcast boxes
             for (let i = 0; i < response.list.length; i) {
             $('#five-day-forecast').append(Weather.renderForcastBox(response.list[i]));
-            i = i + 8;
+            i = i + 8; // five day forcast is a three hourly forecast with 8 forcasets per day
             }
         }) 
         .catch(error => {
-          Weather.handleError(error.responseJSON.message);
+          Weather.handleError(error.responseJSON.message); // if an error occurs call handleError fucntion with the error message
         })
       },
 
@@ -64,7 +68,7 @@ const Weather = {
           $('#main-uvindex').text(response.value);
           var uvclass = ""
           var uvvale = response.value;
-          if(uvvale < 2.9) { uvclass = 'green'; }
+          if(uvvale < 2.9) { uvclass = 'green'; } /* Color code according to UV index */
           else if(uvvale > 3 && uvvale < 4.9) { uvclass = 'yellow'; }
           else if(uvvale > 5 && uvvale < 7.9) { uvclass = 'orange'; }
           else if(uvvale > 8 && uvvale < 10.9) { uvclass = 'red'; }
@@ -100,7 +104,7 @@ const Weather = {
       },
 
       saveHistory : function(city){
-        if(!this.history.includes(city) && city !="") {  
+        if(!this.history.includes(city) && city !="") {  /*If city is already in array do not add again*/
           this.history.push(city);
           localStorage.setItem('weather_dashboard', JSON.stringify(this.history));
         }
